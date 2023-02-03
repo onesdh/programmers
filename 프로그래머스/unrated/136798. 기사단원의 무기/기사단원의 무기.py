c44928272eb4ade0b6e8fe294d4d85f7b6ae42c5
onesdh <80637683+onesdh@users.noreply.github.com>
2023-02-03 16:27:45 +0900

    [unrated] Title: 기사단원의 무기, Time: 4.20 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.py
@@ -0,0 +1,22 @@
+def sol(num) :
+    cnt = 0
+    for i in range(1, int(num**(1/2))+1) :
+        if num == i**2 :
+            cnt+=1
+        elif num % i == 0 :
+            cnt +=2
+    return cnt
+
+def solution(number, limit, power):
+    answer = 0
+
+    for i in range(1, number+1) :
+        k = sol(i)
+        if limit < k :
+            answer += power
+        else :
+            answer += k
+
+    return answer
+
+  
