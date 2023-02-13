2755271c1ef756fd0b7804e3a82be9b417437cce
onesdh <80637683+onesdh@users.noreply.github.com>
2023-02-13 09:03:42 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
@@ -1,11 +1,11 @@
 def solution(n,a,b):
-    answer = 0
+    answer = 1
     # k = [a, b]
     # m = min(k)
     # if m % 2 == 1 and abs(a-b) == 1 :
     #     return answer
 
-    while a != b :
+    while (a+1)//2 != (b+1)//2 :
         answer += 1
         a, b = (a+1)//2, (b+1)//2
     return answer
