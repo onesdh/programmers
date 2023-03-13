3d65b50b8fb3c05da6fd6074f44a013d4e44557e
onesdh <80637683+onesdh@users.noreply.github.com>
2023-03-13 22:24:23 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 36.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -0,0 +1,13 @@
+import math
+def solution(n, m):
+    answer = []
+    for i in range(min(n, m), 0, -1) :
+        if n % i == 0 and m % i == 0 :
+            a = i
+            break
+
+    for i in range(max(n, m) , (n*m)+1) :
+        if i % n == 0 and i % m == 0 :
+            b = i
+            break
+    return [a, b]
