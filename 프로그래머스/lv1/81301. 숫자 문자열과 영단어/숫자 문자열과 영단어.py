17f9e7282decc8736be99eec28159ce61cf8276d
onesdh <80637683+onesdh@users.noreply.github.com>
2023-03-17 16:27:22 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,44 +1,7 @@
 def solution(s):
-    answer = ""
-    i = 0
-    while i != len(s) :
-        
-        if s[i].isdigit() == True: 
-            answer += s[i]
-            i += 1
-        else :
-            if s[i] == "z" :
-                answer += "0"
-                i += 4
-            elif s[i] == "o" :
-                answer += "1"
-                i += 3
-            elif s[i] == "t" :
-                if s[i+1] =="w" :
-                    answer += "2"
-                    i += 3
-                else :
-                    answer += "3"
-                    i += 5
-            elif s[i] == "f" :
-                if s[i+1] =="o" :
-                    answer += "4"
-                    i += 4
-                else :
-                    answer += "5"
-                    i += 4
-            elif s[i] == "s" :
-                if s[i+1] =="i" :
-                    answer += "6"
-                    i += 3
-                else :
-                    answer += "7"
-                    i += 5
-            elif s[i] == "e" :
-                answer += "8"
-                i += 5
-            elif s[i] == "n" :
-                answer += "9"
-                i += 4
-                
-    return int(answer)
+    words = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+    for i in range(len(words)):
+        s = s.replace(words[i], str(i))
+
+    return int(s)
