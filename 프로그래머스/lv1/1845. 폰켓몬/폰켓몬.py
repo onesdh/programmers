f78bcbcf741aeb1203d0c60bb3e2787ec76ac914
onesdh <80637683+onesdh@users.noreply.github.com>
2023-03-21 17:06:48 +0900

    [level 1] Title: 폰켓몬, Time: 0.89 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -4,7 +4,5 @@ def solution(nums):
     for i in nums :
         if i not in k :
             k.append(i)
-    answer = len(k)
-    if answer > len(nums)//2 :
-        answer = len(nums)//2
+    answer = min(len(k), len(nums)//2)
     return answer
